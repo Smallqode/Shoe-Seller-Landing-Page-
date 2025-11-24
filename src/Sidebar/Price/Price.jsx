ffe6b9afc92192bc1860914217384adfdc4802b0
Smallqode <saugataghosh23@gmail.com>
2025-11-24 23:30:58 +0530

    Before Refactoring

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import "./Price.css"
 
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test2'/>
+        <span className='checkmark'></span>All
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test2'/>
+        <span className='checkmark'></span>$0 - $50
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test2'/>
+        <span className='checkmark'></span>$51 - $100
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test2'/>
+        <span className='checkmark'></span>$100 - $150
+      </label>
+
+      <label className='sidebar-label-container'>
+        <input type="radio" name='test2'/>
+        <span className='checkmark'></span>Over $150
+      </label>
+    </div>
   )
 }
 
