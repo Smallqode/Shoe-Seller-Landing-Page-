8da81f691a8e1e0cc7a8fd70c56fb36467b34b64
Smallqode <saugataghosh23@gmail.com>
2025-11-25 23:48:22 +0530

    Small improvement

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,40 +1,17 @@
 import React from 'react'
 import "./Colors.css"
+import Input from '../../Components/Input'
 
 const Colors = () => {
   return (
     <div className="ml">
       <h2 className="sidebar-label-container color-title">Color</h2>
-
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2'/>
-        <span className='checkmark'></span>All
-      </label>
-
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2'/>
-        <span className='checkmark'></span>Black
-      </label>
-
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2'/>
-        <span className='checkmark'></span>Blue
-      </label>
-
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2'/>
-        <span className='checkmark'></span>Red
-      </label>
-
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2'/>
-        <span className='checkmark'></span>Green
-      </label>
-
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2'/>
-        <span className='checkmark'></span>White
-      </label>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
     </div>
   )
 }
